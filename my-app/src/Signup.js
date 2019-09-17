d414dd7cf8caf207cd98891b4fed7722d712b542
rsmantri <rishab.mantri@gmail.com>
2019-09-16 20:47:52 -0700

    Basic signup page updated.

---
+++ my-app/src/Signup.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import {
+    HelpBlock,
+    FormGroup,
+    FormControl,
+    ControlLabel
+} from "react-bootstrap";
+
+export default class Signup extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: false,
+            email: "",
+            password: "",
+            confirmPassword: "",
+            confirmationCode: "",
+            newUser: null
+        };
+    }
+
+    validateForm() {
+        return (
+            this.state.email.length > 0 &&
+            this.state.password.length > 0 &&
+            this.state.password === this.state.confirmPassword
+        );
+    }
+
+    renderForm() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                </FormGroup>
+                <FormGroup controlId="confirmPassword" bsSize="large">
+                    <ControlLabel>Confirm Password</ControlLabel>
+                    <FormControl
+                        value={this.state.confirmPassword}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                </FormGroup>
+                <LoaderButton
+                    block
+                    bsSize="large"
+                    disabled={!this.validateForm()}
+                    type="submit"
+                    isLoading={this.state.isLoading}
+                    text="Signup"
+                    loadingText="Signing up…"
+                />
+            </form>
+        );
+    }
+
+    render() {
+        return (
+            <div className="Signup">
+                {this.state.newUser === null
+                    ? this.renderForm()
+                    : this.renderConfirmationForm()}
+            </div>
+        );
+    }
+}
