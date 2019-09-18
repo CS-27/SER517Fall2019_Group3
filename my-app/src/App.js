40103cd2ed6b907ada8fd84aa21e21158b54c1da
HarshitaKAM <hkajal@asu.edu>
2019-09-17 22:25:55 -0700

    US:7 Task:68 Adding routing option to visit and test the landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import Contact from './components/Contact';
 
-function App() {
+class App extends Component {
+  render(){ 
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Welcome to Brew Day!
         <p>
+        <Link to={'/contact'} className="nav-link">Contact</Link>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Switch>
+              <Route path='/contact' component={Contact} />
+          </Switch>
       </header>
     </div>
+    </Router>
   );
+}
 }
 
 export default App;
---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Component } from 'react';
+
+class ContactUs extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Contact</h2>
+        </div>
+    );
+  }
+}
+
+function Contact() {
+  return (
+    <div className="Contact">
+    Contact us here
+    </div>
+  );
+}
+
+export default Contact;
