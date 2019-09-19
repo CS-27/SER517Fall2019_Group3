b2e59517a6a5086765fcea3f973cb61f5717c557
CS-27 <chittineni.shalini@gmail.com>
2019-09-18 19:11:19 -0700

    US:4 Task:72 Adding React containers to homepage

---
+++ my-app/src/containers/Homepage/homepage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Aux from '../../HigherOrderComponent/Auxillary';
+
+class Homepage extends Component{
+    render(){
+
+        return(
+            <Aux>
+                <div> Homepage</div>
+                <div> Controls</div>
+            </Aux>
+
+        );
+    }
+
+}
+
+export default Homepage;
