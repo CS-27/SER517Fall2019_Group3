4560ffa68a7eec101013bc902870ee5ce3271083
CS-27 <chittineni.shalini@gmail.com>
2019-09-18 19:15:35 -0700

    US:4 Task:72 Added code for homepage layout.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React,  {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
-function App() {
+import Homepage from './containers/Homepage/homepage';
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Welcome to Brew Day!
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Layout>
+        <Homepage> </Homepage>
+      </Layout>
     </div>
   );
 }
+}
+
+//old
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         Welcome to BrewDay 2!
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
