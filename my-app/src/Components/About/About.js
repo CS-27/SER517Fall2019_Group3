a50aa9a2070814a3adef3e8404c4123546c085a5
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-09-20 13:25:18 -0700

    US:6 Task:70  Adding content and css to about page

--- my-app/src/Components/About/About.js
+++ my-app/src/Components/About/About.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react';
+import React from 'react';
 import './About.css';
 
 /*Author: Jahnavi Bantupalli
@@ -7,14 +7,16 @@ Date modified : Sept 20, 2019
 */
 function About() {
   return (
-    <div className="about">
-      <header className="about-header">
+    <div>
+      <header className="aboutUs">
         About Us!
       </header>
+      
       <h1>
           Welcome Home Brewers!
           We support Home Brewers to store information about the brews.
       </h1>
+    
     </div>
     
   );
