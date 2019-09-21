02ff433f8bc82c20431499c8386a89af2816da41
HarshitaKAM <hkajal@asu.edu>
2019-09-21 13:44:22 -0700

    US:7 Task:62 Adding a basic webpage for Contact Us

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,22 +1,37 @@
+/*Author: Harshita Kajal
+Date added: Sep 17, 2019
+Date modified : Sept 21, 2019
+*/
+
 import React from 'react';
 import { Component } from 'react';
-
-class ContactUs extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Contact</h2>
-        </div>
-    );
-  }
-}
+import './Contact.css';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image'
+import { Container, Row, Col } from 'react-bootstrap';
+//import CardDeck from 'react-bootstrap/CardDeck';
 
 function Contact() {
   return (
-    <div className="Contact">
-    Contact us here
-    </div>
-  );
+  	<div>
+      <header className="Contact">
+        Contact Us
+        </header>
+      
+     <Card  className="card-main">
+  <Card.Body>
+    <Card.Text className ="card-body">
+       Questions about brewing? Questions on how to use the App? Suggestions for the App? or just want to grab some beer with us
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+<div>
+        <h2 className = "Contact"> <center>Contact Us</center></h2>
+        
+</div>
+</div>
+);
 }
 
 export default Contact;
