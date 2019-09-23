2256bccf712e47debbae7fe4b9b8fee5f677ec28
HarshitaKAM <hkajal@asu.edu>
2019-09-22 23:36:53 -0700

    US:7 Task:62 Adding cards to the web page

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,37 +1,62 @@
 /*Author: Harshita Kajal
 Date added: Sep 17, 2019
-Date modified : Sept 21, 2019
+Date modified : Sept 22, 2019
 */
 
-import React from 'react';
-import { Component } from 'react';
-import './Contact.css';
-import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image'
-import { Container, Row, Col } from 'react-bootstrap';
-//import CardDeck from 'react-bootstrap/CardDeck';
+  import React from "react";
+  import "./style-card.css";
+  import {Row, Col } from 'react-bootstrap';
+  import img1 from "./email.jpg";
+  import img2 from "./Call.png";
 
-function Contact() {
-  return (
-  	<div>
-      <header className="Contact">
-        Contact Us
-        </header>
-      
-     <Card  className="card-main">
-  <Card.Body>
-    <Card.Text className ="card-body">
-       Questions about brewing? Questions on how to use the App? Suggestions for the App? or just want to grab some beer with us
-    </Card.Text>
-  </Card.Body>
-</Card>
+  function Contact() {
+    return (
+      <div className= 'Contact'>
+      <header align='center'><h1>Contact Us</h1></header>
+      <Row>
+      <Col xs={6} md={4} sm={3}>
+      <div className='card shadow'>
+        <div className='overflow'>
+          <img src={img1} alt='Image 1' className='card-img-top' />
+        </div>
+        <div className='card-body text-dark'>
+          <h4 className='card-title' align='center'>Email US</h4>
+          <p className='card-content text-secondary'>
+            beer_for_life@gmail.com
+          </p>
+        </div>
+        </div>
+        </Col>
+    <Col xs={6} md={4} sm={3}>
+    <div className='card shadow'>
+        <div className='overflow'>
+          <img src={img2} alt='Image 1' className='card-img-top' />
+        </div>
+        <div className='card-body text-dark'>
+          <h3>Call US</h3>
+          <p className='card-content text-secondary'>
+            Google Maps here
+          </p>
+        </div>
+        </div>
+        </Col>
 
-<div>
-        <h2 className = "Contact"> <center>Contact Us</center></h2>
-        
-</div>
-</div>
-);
-}
-
-export default Contact;
+        <Col xs={6} md={4} sm={3}>
+    <div className='card text-center shadow'>
+        <div className='overflow'>
+          <img src={img1} className='card-img-top' />
+        </div>
+        <div className='card-body text-dark'>
+          <h4 className='card-title'>Visit US</h4>
+          <p className='card-content text-secondary'>
+            Maps + Address coming soon
+          </p>
+        </div>
+        </div>
+        </Col>
+        </Row>
+      </div>
+    );
+  };
+   
+  export default Contact;
