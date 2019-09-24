2454256a298196a9264e62803c8b8b6e68375c04
CS-27 <chittineni.shalini@gmail.com>
2019-09-23 19:41:04 -0700

    US:8 Task:4,72 Adding toolbar and other components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
+import Backdrop from './components/Backdrop';
+import Toolbar from './components/Toolbar';
+
+class App extends Component {
+  render(){
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Welcome to Brew Day!
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </header>
     </div>
+    </Router>
   );
 }
+}
 
-export default App;
+export default () => <Toolbar />;
+
+//export default () => <Backdrop />;
+
+
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         Welcome to Brew Day!
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
