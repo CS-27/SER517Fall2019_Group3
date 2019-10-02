ee371edbb730ca75e6610d061ab2479b6fb59cda
Srajangpt1 <sgpt18@gmail.com>
2019-10-01 18:39:43 -0700

    US#37 Task#80 Created api endpoint to fetch brewer's equipment

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,7 @@
 
 from flask import Flask, jsonify, request, Blueprint
+import recipeFunctions
+import equimentFunctions
 
 
 brewDay_api = Flask(__name__)
@@ -9,15 +11,20 @@ def indexPage():
 	return 'Hello, This is Brew Day'
 
 @brewDay_api.route('/showRecipe', methods = ['GET'])
-def showRecipe(name):
-	recipeName = request.args.get('name')
+def showRecipe():
+	recipeName = request.args.get('recipeName')
 	return jsonify({'RecipeInfo': recipeFunctions.showRecipeByName(recipeName)})
 
 
 @brewDay_api.route('/addRecipe', methods = ['POST'])
-def addRecipeInfo(recipeInfo):
+def addRecipeInfo():
 	recipeInfo = request.args.get('recipeInfo')
 	return jsonify({'recipeAdditionStatus' : recipeFunctions.addRecipe(recipeInfo)})
+
+@brewDay_api.route('/showEquipment', methods = ['GET'])
+def showEquiment():
+	userID = request.args.get('userID')
+	return jsonify({'equipmentList' : equimentFunctions.showEquipment(userID)})
 
 if __name__ == '__main__':
     brewDay_api.run(debug=True)
