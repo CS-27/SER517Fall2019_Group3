d18acba8a1376b858721058ef3a39ea1dacb0e41
HarshitaKAM <hkajal@asu.edu>
2019-10-07 23:00:36 -0700

    US:40 Task:77 Adding input options for recipe

--- my-app/src/components/AddRecipe.js
+++ my-app/src/components/AddRecipe.js
@@ -3,36 +3,49 @@ Date added: Sep 29, 2019
 Date modified : Sept 30, 2019
 */
 
-import React from "react"
+import React, {Component} from "react"
 import RecipeDetails from "./RecipeDetails"
-import { FormLabel } from "react-bootstrap"
-class Form extends React.Component {
-  state = {
+import { FormLabel, FormControl, FormGroup } from "react-bootstrap"
+class Form extends Component {
+    constructor(props) {
+        super(props);
+    this.state = {
     object1: [{name:""}],
     HOV: "",
     description: ""
-  }
+  };
+}
 handleChange = (e) => {
     if (["name"].includes(e.target.className) ) {
       let object1 = [...this.state.object1]
-      object1[e.target.dataset.id][e.target.className] = e.target.value()
+      object1[e.target.dataset.id][e.target.className] = e.target.value
       this.setState({ object1 }, () => console.log(this.state.object1))
     } else {
-      this.setState({ [e.target.name]: e.target.value() })
+      this.setState({ [e.target.name]: e.target.value})
     }
   }
+  
+handleSubmit = (e) => { e.preventDefault() }
 addValues = (e) => {
     this.setState((prevState) => ({
       object1: [...prevState.object1, {name:""}],
     }));
   }
-handleSubmit = (e) => { e.preventDefault() }
+
 render() {
     let {HOV, description, object1} = this.state
     return (
       <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
-        <FormLabel htmlFor="name">HOV</FormLabel>
-        <input type="text" name="HOV" id="HOV" value={HOV} />
+        <FormGroup bsSize="large">
+                    <FormLabel color="white" htmlFor="name">HOV</FormLabel>
+                    <FormControl
+                        autoFocus
+                        type="text"
+                        value={HOV}
+                        name="HOV" 
+                        id="HOV"  
+                    />
+                </FormGroup>  
         <FormLabel htmlFor="description">Description</FormLabel>
         <input type="text" name="description" id="description" value={description} />
         <button onClick={this.addValues}>Add more</button>
--- my-app/src/components/RecipeDetails.js
+++ my-app/src/components/RecipeDetails.js
@@ -2,9 +2,10 @@
 Date added: Sep 29, 2019
 Date modified : Sept 30, 2019
 */
-
 import React from "react"
-const RecipeDetails = (props) => {
+
+const RecipeDetails = (props) => 
+{
   return (
     props.object1.map((key, idx)=> {
       let id1 = `cat-${idx}`
@@ -13,15 +14,16 @@ const RecipeDetails = (props) => {
           <label htmlFor={id1}>{`Ingredient1${idx + 1}`}</label>
           <input
             type="text"
-            name={id1}
-            value-id={idx}
-            id={id1}
-            value={props.object1[idx].name} 
             className="name"
-          />
+            name={id1}
+            id={id1}
+            value-id={idx}
+            value={props.object1[idx].name} 
+            />
         </div>
       )
     })
   )
 }
+
 export default RecipeDetails
