108f9d49ecdd2882245d54a155519b058ba6a906
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-10-09 09:59:12 -0700

    US:25 Task: 105 Add ingredient page

---
+++ my-app/src/components/ListIngredients/listingredient.js
@@ -0,0 +1,64 @@
+/*Author: Jahnavi Bantupalli
+Date added: Oct 9, 2019
+*/
+import React, { Component } from "react";
+
+import './listingredient.css';
+import Card from 'react-bootstrap/Card';
+import { Container, Row, Col } from 'react-bootstrap';
+export default class IngredientPage extends Component {
+    constructor(props) {
+        super(props);
+        this.message = ""
+        this.state = {
+            // isLoading: false,
+            name:"",
+            quantity:"",
+            user: null
+        };
+
+        
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit=(event)=> {
+        console.log(this.state);
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', 'http://127.0.0.1:5000/')
+
+        event.preventDefault();
+      }
+
+
+    renderList() {
+        return (
+            
+            <Container>
+                <span class="iconify" data-icon="mdi-bottle-wine" data-inline="false"></span>
+            <Card  className="mainCard">
+         <Card.Body className = "card-body">
+         <Card.Title className="titleCard" >List ingredient </Card.Title>
+
+      
+         </Card.Body>
+       </Card>
+            </Container>
+            
+            
+          
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                { this.renderList()}
+            </div>
+        );
+    }
+}
