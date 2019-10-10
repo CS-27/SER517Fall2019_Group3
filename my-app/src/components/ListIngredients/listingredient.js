1e7eaeae1ce8c1cb976b7d4dbeccb939ec992715
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-10-09 20:23:54 -0700

    US:25 Task: 107  create a list of ingredients

--- my-app/src/components/ListIngredients/listingredient.js
+++ my-app/src/components/ListIngredients/listingredient.js
@@ -2,20 +2,20 @@
 Date added: Oct 9, 2019
 */
 import React, { Component } from "react";
-
+import { Button} from "react-bootstrap";
 import './listingredient.css';
 import Card from 'react-bootstrap/Card';
+import axios from "axios";
+
 import { Container, Row, Col } from 'react-bootstrap';
 export default class ListIngredient extends Component {
     constructor(props) {
         super(props);
-        this.message = ""
-        this.state = {
-            // isLoading: false,
-            name:"",
-            quantity:"",
-            user: null
-        };
+    this.state = {
+      error: null,
+      ingredients: [],
+      response: {}
+    }
 
         
     }
@@ -34,6 +34,52 @@ export default class ListIngredient extends Component {
         event.preventDefault();
       }
 
+      getIngredients(){
+            console.log("ingredients")
+            var apiUrl = 'http://127.0.0.1:5000/showIngredient?userID=user1'
+            axios.get('http://127.0.0.1:5000/showIngredient?userID=user1').
+            then(response=> {
+            //    var data = response.json()
+            //   console.log(response);
+
+            
+            
+            });
+
+            fetch(apiUrl)
+            .then(res => res.json())
+            .then(
+              (result) => {
+                  var data =JSON.parse(result['IngredientList']);
+
+               var ingredients =[];
+               console.log(data);
+               Object.keys(data).forEach(function(key) {
+                   if(key!="userID" && key!="_id")
+                        ingredients.push(data[key]);
+              });
+            //    for (let i = 1; i <=3; i++) {
+            //         var key = 'ingredient'+i;
+            //        ingredients.push({
+            //            key: data[key]
+            //        });
+            //     //    this.setState({
+            //     //     ingredients: ingredients
+            //     //   });
+            //    }
+               console.log(ingredients)
+
+              },
+              (error) => {
+                this.setState({ error });
+              }
+            )
+           
+
+      }
+
+
+
 
     renderList() {
         return (
@@ -43,7 +89,7 @@ export default class ListIngredient extends Component {
             <Card  className="mainCard">
          <Card.Body className = "card-body">
          <Card.Title className="titleCard" >List ingredient </Card.Title>
-
+        <Button onClick ={this.getIngredients} ></Button>
       
          </Card.Body>
        </Card>
