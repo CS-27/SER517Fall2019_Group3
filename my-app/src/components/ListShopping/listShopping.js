e0f25af2973ed1b55b91248961b749f3d2639601
rsmantri <rishab.mantri@gmail.com>
2019-10-10 19:54:47 -0700

    implemented Display shopping list

--- my-app/src/components/ListShopping/listShopping.js
+++ my-app/src/components/ListShopping/listShopping.js
@@ -3,19 +3,23 @@ import React, { Component } from "react";
 
 import './listShopping.css';
 import Card from 'react-bootstrap/Card';
+import DataTable from '../ListIngredients/datatable';
 import { Container, Row, Col } from 'react-bootstrap';
+import Loader from 'react-loader-spinner';
 export default class ListShopping extends Component {
     constructor(props) {
         super(props);
-        this.message = ""
         this.state = {
-            // isLoading: false,
-            name:"",
-            quantity:"",
-            user: null
-        };
+            error: null,
+            ingredients: [],
+            response: {}
+        }
+        this.ingredients = null;
+        this.loading = true;
+        this.getIngredients();
 
-        
+
+
     }
 
     handleChange = event => {
@@ -32,6 +36,44 @@ export default class ListShopping extends Component {
         event.preventDefault();
       }
 
+    getIngredients=()=> {
+        var apiUrl = 'http://127.0.0.1:5000/showShoppingList?userID=user1'
+
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result['ShoppingList']);
+                    var data = result['ShoppingList'];
+
+                    this.loading = false;
+                    var ingredients = [];
+
+                    Object.keys(data).forEach(function (key) {
+                        if (key != "userID" && key != "_id") {
+                            ingredients.push([
+                                key, data[key]
+                            ]);
+                        }
+
+                    });
+
+                    this.setState({
+                        ingredients: ingredients
+                    });
+
+
+                    const ingrarray = Object.keys(this.state.ingredients).map(i => this.state.ingredients[i])
+                    this.ingredients = ingrarray;
+                    console.log(this.ingredients[0]);
+                },
+                (error) => {
+                    this.setState({error});
+                }
+            )
+
+    }
+
 
     renderList() {
         return (
@@ -41,8 +83,17 @@ export default class ListShopping extends Component {
             <Card  className="mainCard">
          <Card.Body className = "card-body">
          <Card.Title className="titleCard" >Shopping List </Card.Title>
+             {this.loading ?       <Loader
+                 type="Circles"
+                 color="#00BFFF"
+                 height={100}
+                 width={100}
+                 timeout={3000} //3 secs
 
-      
+             />: <DataTable items={this.state.ingredients}></DataTable>}
+
+
+
          </Card.Body>
        </Card>
             </Container>
