3cc572c8d912bc1a993099c487c96fb88ea4ac4e
CS-27 <chittineni.shalini@gmail.com>
2019-10-10 18:07:08 -0700

    US:31 Task:108 Added code to display equipment list

--- my-app/src/components/EquipmentPage/equipmentList.js
+++ my-app/src/components/EquipmentPage/equipmentList.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 import './equipmentList.css';
 import Card from 'react-bootstrap/Card';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import DataTable from '../ListIngredients/datatable';
+import Loader from 'react-loader-spinner';
 export default class equipmentList extends Component {
      constructor(props) {
         super(props);
@@ -18,84 +20,95 @@ export default class equipmentList extends Component {
           equipment: [],
           response: {}
         }
-
+        this.equipment = null;
+        this.loading = true;
+        this.getEquipment();
         
     }
 
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    }
+    // handleChange = event => {
+    //     this.setState({
+    //         [event.target.id]: event.target.value
+    //     });
+    // }
 
     handleSubmit=(event)=> {
-        console.log(this.state);
+        //console.log(this.state);
         var xhr = new XMLHttpRequest()
         xhr.open('POST', 'http://127.0.0.1:5000/')
 
         event.preventDefault();
       }
 
-      getEquipment(){
-            console.log("equipment")
-            var apiUrl = 'http://127.0.0.1:5000/showEquipment?userID=user1'
-            // axios.get('http://127.0.0.1:5000/showEquipment?userID=user1').
-            // then(response=> {
-            // //    var data = response.json()
-            // //   console.log(response);
-
-            
-            
-            // });
-
+      getEquipment=()=>{
+          var apiUrl = 'http://127.0.0.1:5000/showEquipment?userID=user1'
+          
             fetch(apiUrl)
             .then(res => res.json())
             .then(
               (result) => {
-                  var data =JSON.parse(result['equipmeneList']);
-
-               console.log(data['equip1']);
+                  var data =JSON.parse(result['equipmentList']);
+               this.loading = false;
                var equipment =[];
-               for (let i = 0; i < 3; i++) {
-                    var key = 'equip'+i;
-                    equipment.concat(key);
-               }
-               console.log(this.state.equipment);
+               
+               Object.keys(data).forEach(function(key) {
+                   if(key!="userID" && key!="_id"){
+                    equipment.push([
+                      key,data[key]
+                   ]);
+                   }
+                       
+              });
+             
+                   this.setState({
+                    equipment: equipment
+                  });
 
-              },
-              (error) => {
-                this.setState(error);
-              }
-            )
-      }
-
-
-
-
-    renderList() {
-        return (
-            
-            <Container>
-                <span class="iconify" data-icon="mdi-bottle-wine" data-inline="false"></span>
-            <Card  className="mainCard">
-         <Card.Body className = "card-body">
-         <Card.Title className="titleCard" >Equipment List</Card.Title>
-        <Button onClick ={this.getEquipment} ></Button>
-      
-         </Card.Body>
-       </Card>
-            </Container>
-            
-            
+                  const dataArray = Object.keys(this.state.equipment).map(i => this.state.equipment[i])
+                  this.equipment = dataArray;
+                  console.log(this.equipment[0]);
+                 },
+                 (error) => {
+                   this.setState({ error });
+                 }
+               )
+              
+   
+         }
+   
+         renderList() {
+            return (
+                
+                <Container>
+                    <span class="iconify" data-icon="mdi-bottle-wine" data-inline="false"></span>
+                <Card  className="mainCard">
+             <Card.Body className = "card-body">
+             <Card.Title className="titleCard" >List of Equipment</Card.Title>
+             {this.loading ?       <Loader
+             type="Circles"
+             color="#00BFFF"
+             height={100}
+             width={100}
+             timeout={3000} //3 secs
+    
+          />: <DataTable items={this.state.equipment}></DataTable>}
+    
+    
           
-        );
+             </Card.Body>
+           </Card>
+                </Container>
+                
+                
+              
+            );
+        }
+    
+        render() {
+            return (
+                <div>
+                    { this.renderList()}
+                </div>
+            );
+        }
     }
-
-    render() {
-        return (
-            <div>
-                { this.renderList()}
-            </div>
-        );
-    }
-}
