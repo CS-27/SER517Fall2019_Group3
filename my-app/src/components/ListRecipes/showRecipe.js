874e1de41b74d52ba95e7020581420f7df2a0209
HarshitaKAM <hkajal@asu.edu>
2019-10-18 18:45:20 -0700

    US:45 Task:130 Editing submission options

--- my-app/src/components/ListRecipes/showRecipe.js
+++ my-app/src/components/ListRecipes/showRecipe.js
@@ -27,7 +27,12 @@ export default class recipeList extends Component {
         
     }
 
-    //handle submit
+    handleSubmit=(event)=> {
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', 'http://127.0.0.1:5000/')
+    
+        event.preventDefault();
+      }
 
 
       //gethere
