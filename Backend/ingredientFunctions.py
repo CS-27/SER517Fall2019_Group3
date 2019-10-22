5ed9c7bcb82ed9fdd219062052dd8e906d45a08d
Srajangpt1 <sgpt18@gmail.com>
2019-10-22 06:04:29 -0700

    US109 Task#140 api endpoint to delete ingrdient

--- Backend/ingredientFunctions.py
+++ Backend/ingredientFunctions.py
@@ -11,7 +11,7 @@ def showIngredient(userID):
 	collection = db.userIngredient
 
 	result = collection.find_one({'userID' : userID})
-	print (json.dumps(result, default=json_util.default))
+	#print (json.dumps(result, default=json_util.default))
 	return json.dumps(result, default=json_util.default)
 
 def addIngredient(userIngList):
@@ -55,10 +55,22 @@ def updateIngredientQuantity(userID, ingList):
 	else:
 		return False
 
+def deleteIngredient(userID, ingList):
+	client = pymongo.MongoClient("mongodb://test1:project2019@gettingstarted-shard-00-00-2kb0f.mongodb.net:27017,gettingstarted-shard-00-01-2kb0f.mongodb.net:27017,gettingstarted-shard-00-02-2kb0f.mongodb.net:27017/ingredient?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin&retryWrites=true&w=majority")
+	db = client.ingredient
 
+	collection = db.userIngredient
 
-#c = {'userID' : 'user1', 'ingredient1':'x', 'ingredient2' : 'y', 'ingredient3':'t'}
-#addRecipe(c)
-#addIngredient(c)
+	result = collection.find_one({'userID': userID})
+	#print result
+	search_query = { "userID": userID }
+	if result:
+		for key,value in ingList.items():
+			search_query = {"$and": [{"userID": userID}, {key: {'$exists':True}}]}
+			#print collection.find_one(search_query)
+			updateCollection = collection.update(search_query, {'$unset' : {key:1}})
+			#print updateCollection
+		return True
+	return False
 
 
--- Backend/main.py
+++ Backend/main.py
@@ -117,12 +117,25 @@ def userRegister():
 def updateIngredient():
 	req_data = request.get_json(force = True)
 	ingList = {}
-	print req_data
+	#print req_data
 	for key,value in req_data.items():
 		ingList.__setitem__(key,value)
 	userID = ingList['userID']
 	del ingList['userID']
 	response = jsonify({'Ingredient Update status' : ingredientFunctions.updateIngredientQuantity(userID,ingList)})
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
+
+@brewDay_api.route('/deleteIngredient', methods = ['POST'])
+def deleteIngredient():
+	req_data = request.get_json(force = True)
+	ingList = {}
+	print req_data
+	for key,value in req_data.items():
+		ingList.__setitem__(key,value)
+	userID = ingList['userID']
+	del ingList['userID']
+	response = jsonify({'Ingredient delete status' : ingredientFunctions.deleteIngredient(userID,ingList)})
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	return response
 
