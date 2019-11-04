578dba368986855d4f0cd9d3a5894f7350d62fec
Srajangpt1 <sgpt18@gmail.com>
2019-11-04 16:22:16 -0700

    US#41 Task#202 adding api endpoint to view user specific recipes

--- Backend/main.py
+++ Backend/main.py
@@ -104,6 +104,15 @@ def recipeSearch():
 	return response
 
 
+@brewDay_api.route('/viewMyRecipes', methods = ['GET'])
+def viewMyRecipes():
+	req_data = request.args.get('userID')
+	response = jsonify({'My Recipe List' : json.loads(recipeFunctions.viewUserRecipe(req_data))})
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
+
+
+
 # # # # Equipment Functions # # # # 
 
 
