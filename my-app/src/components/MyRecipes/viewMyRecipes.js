3e336027177b30088b59a515c43cff9ba5b8f2f0
CS-27 <chittineni.shalini@gmail.com>
2019-11-06 23:15:35 -0700

    US:41 Task:189 Making changes to list display of recipes

--- my-app/src/components/MyRecipes/viewMyRecipes.js
+++ my-app/src/components/MyRecipes/viewMyRecipes.js
@@ -3,10 +3,12 @@ import React, { Component } from "react";
 
 import './viewMyRecipes.css';
 import Card from 'react-bootstrap/Card';
-import { Container, Row, Col, Button,Form,
+import {
+    Container, Row, Col, Button, Form,
     FormGroup,
     FormControl,
-    FormLabel, } from 'react-bootstrap';
+    FormLabel,
+} from 'react-bootstrap';
 import Loader from 'react-loader-spinner';
 import ListRecipeDatatable from "./myRecipeListDatatable";
 export default class ViewMyRecipe extends Component {
@@ -16,13 +18,13 @@ export default class ViewMyRecipe extends Component {
             error: null,
             items: [],
             response: {},
-            name:"",
-            names:[]
+            name: "",
+            names: []
         }
         this.items = null;
         this.loading = true;
-        this.names=null;
-        this.name=null;
+        this.names = null;
+        this.name = null;
 
     }
 
@@ -32,122 +34,121 @@ export default class ViewMyRecipe extends Component {
         });
     }
 
-    handleSubmit=(event)=> {
+    handleSubmit = (event) => {
         //console.log(this.state);
         var xhr = new XMLHttpRequest()
         xhr.open('POST', 'http://127.0.0.1:5000/')
 
         event.preventDefault();
-      }
+    }
 
-    getItems=(event)=> {
-        var apiUrl = 'http://127.0.0.1:5000/viewMyRecipes?userID='+sessionStorage.getItem("username");
+    getItems = (event) => {
+        var apiUrl = 'http://127.0.0.1:5000/viewMyRecipes?userID=' + sessionStorage.getItem("username");
 
         fetch(apiUrl)
             .then(res => res.json())
             .then(
                 (result) => {
                     var data = result['My Recipe List'];
-                    var names=[];
+                    var names = [];
 
                     this.loading = false;
                     this.items = [result['My Recipe List']];
-                    
+
                     console.log(this.items);
                     console.log(data.length);
 
-                    
-            for(var i=0;i<this.items.length;i++)
-            {
 
-                                this.items[i].map((values)=>{
-                                
-                                        names.push([
-                                        values.name
-                                            ]);
-                                        
-                                            
-                                })
-                                
-            }
-                    
+                    for (var i = 0; i < this.items.length; i++) {
+
+                        this.items[i].map((values) => {
+
+                            names.push([
+                                values.name
+                            ]);
+
+
+                        })
+
+                    }
+
+                    // this.state.names=names
+                    // this.state.items=items
 
                     this.setState({
                         items: this.items,
                         names: names
                     });
 
-                    //console.log(names);
 
-
-                    },
+                },
                 (error) => {
-                    this.setState({error});
+                    this.setState({ error });
                 }
             )
 
     }
 
     deleteItem = (name) => {
-         const updatedItems = this.state.names.filter(i => i[0] !== name[0]);
-         this.setState({ names: updatedItems })
-        }
-      
-    
-      deleteRecipe =(name)=>{
+        const updatedItems = this.state.names.filter(i => i[0] !== name[0]);
+        this.setState({ names: updatedItems })
+    }
+
+
+    deleteRecipe = (name) => {
         console.log("in delRecipe")
         console.log(name)
         fetch('http://127.0.0.1:5000/deleteRecipeAdmin', {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name: name.toString()
-          })
-          
-        })
-        
-        .then(() => {
-            this.deleteItem(name)
-          })
-          .catch(err => console.log(err))
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name.toString()
+            })
 
-      }
+        })
+
+            .then(() => {
+                this.deleteItem(name)
+            })
+            .catch(err => console.log(err))
+
+    }
 
 
     renderList() {
         return (
-            
+
             <Container>
                 <span class="iconify" data-icon="mdi-bottle-wine" data-inline="false"></span>
-            <Card  className="mainCardOneMain">
-         <Card.Body className = "card-body">
-         <Card.Title className="titleCard" > My Recipes below</Card.Title>
-             <Form onSubmit={this.handleSubmit}>
-                 <Button onClick ={this.getItems} id = "btn-color" variant="primary"  >View Beers</Button>
-             </Form>
-             {this.loading ?       <Loader
-                 type="Circles"
-                 color="#00BFFF"
-                 height={100}
-                 width={100}
-                 timeout={2000} //2 secs
+                <Card className="mainCardOneMain">
+                    <Card.Body className="card-body">
+                        <Card.Title className="titleCard" > My Recipes below</Card.Title>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Button onClick={this.getItems} id="btn-color" variant="primary"  >View Beers</Button>
+                        </Form>
+                        {this.loading ? <Loader
+                            type="Circles"
+                            color="#00BFFF"
+                            height={100}
+                            width={100}
+                            timeout={2000} //2 secs
 
-             /> :
-              <ListRecipeDatatable names={this.state.names} deleteItem={this.deleteItem} deleteRecipe={this.deleteRecipe} ></ListRecipeDatatable>}
+                        /> :
+                            <ListRecipeDatatable names={this.state.names} deleteItem={this.deleteItem} deleteRecipe={this.deleteRecipe} ></ListRecipeDatatable>}
 
-         </Card.Body>
-       </Card>
+                    </Card.Body>
+                </Card>
             </Container>
-          
+
         );
     }
 
     render() {
         return (
             <div>
-                { this.renderList()}
+                {this.renderList()}
             </div>
         );
     }
