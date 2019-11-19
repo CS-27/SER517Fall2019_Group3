1188f7105221e0e80eda744c80290d6809f207f8
HarshitaKAM <hkajal@asu.edu>
2019-11-18 18:00:36 -0700

    US:205 Task:207 Updating logo

--- my-app/src/components/Toolbar/Toolbar.js
+++ my-app/src/components/Toolbar/Toolbar.js
@@ -60,7 +60,7 @@ class ButtonAppBar extends Component {
 
               <div className="root" >
                 <Link to="/">
-                <Image src="/images/logo_brew.png"  color="inherit" className="title" className="text-capitalize"
+                <Image src="/images/logo_new.png"  color="inherit" className="title" className="text-capitalize"
                 style ={{width: 70, height: 70, marginTop:5, marginBottom: 5 }} /> </Link>
                 <div className="dropdown">
                   <Button className="text-capitalize" color="inherit">Users</Button>
